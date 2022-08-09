ce17451d4f61206587fa3b7bc6e67feb19cc1ea2
Alex <al.gorzhanin23@gmail.com>
2022-08-09 18:32:44 +0300

    Fix currency convert service and architecture

--- main/services/currency/interactors.py
+++ main/services/currency/interactors.py
@@ -3,5 +3,7 @@ from datetime import date
 
 
 def create_new_currency_pair_and_delete_old_pairs(pair: str, course: float):
-    CurrencyCourse.objects.filter(pair=pair).delete()
+    old_pairs = CurrencyCourse.objects.filter(pair=pair)
+    if old_pairs:
+        old_pairs.delete()
     CurrencyCourse.objects.create(pair=pair, price=course, date=date.today())
