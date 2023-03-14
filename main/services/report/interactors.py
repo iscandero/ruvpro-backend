d6d390a84ef177a5b6003eea8ec44fae88e16eb2
agorozhanin <al.gorozhanin23@gmail.com>
2023-03-14 15:12:51 +0300

    Create new reports version with html, fix reports names

--- main/services/report/interactors.py
+++ main/services/report/interactors.py
@@ -1,28 +1,19 @@
-from fpdf import FPDF
+import jinja2
+import pdfkit as pdfkit
 
 from main.models import Report
-from ruvpro.settings import STATIC_ROOT
+from ruvpro.settings import STATIC_ROOT, MEDIA_ROOT
 
 
 def create_report_object(user, date, url):
     return Report.objects.create(user=user, date=date, url=url)
 
 
-def create_pdf_report(titles_list: list, data_list: list, save_path: str):
-    pdf = FPDF()
-    pdf.add_font('DejaVu', '', STATIC_ROOT + '/DejaVuSansCondensed.ttf', uni=True)
-    pdf.set_font('DejaVu', '', 10)
-    pdf.add_page()
-    t = len(titles_list) + 0.5
-    col_width = pdf.w / t
-    row_height = pdf.font_size + 2
-    data = [titles_list, *data_list]
-    for row in data:
-        for item in row:
-            if item:
-                item = item[:15]
-            pdf.cell(col_width, row_height,
-                     txt=item, border=1)
-        pdf.ln(row_height)
-
-    pdf.output(save_path)
+def create_pdf_report(titles_list: list, data_list: list, report_name: str):
+    template_loader = jinja2.FileSystemLoader(searchpath=STATIC_ROOT)
+    template_env = jinja2.Environment(loader=template_loader)
+    template_file = "report_template.html"
+    template = template_env.get_template(template_file)
+    output_data = template.render(titles_list=titles_list, data_list=data_list, report_name='Report')
+    pdf_name = MEDIA_ROOT + '/users_reports' + '/' + report_name + '.pdf'
+    pdfkit.from_string(str(output_data), pdf_name, options={"enable-local-file-access": ""})
--- main/services/report/use_cases.py
+++ main/services/report/use_cases.py
@@ -1,6 +1,4 @@
-import string
 from datetime import datetime
-import random
 from operator import itemgetter
 
 from main.const_data.serv_info import SERV_NAME
@@ -11,7 +9,6 @@ from main.services.project.selectors import get_project_by_id
 from main.services.report.interactors import create_pdf_report, create_report_object
 from main.services.work_with_date import convert_timestamp_to_date
 from main.services.worker.selectors import get_all_workers_by_project_id
-from ruvpro.settings import MEDIA_ROOT
 
 
 def create_user_current_project_report(user, project_id=None, from_date=None, to_date=None):
@@ -29,8 +26,19 @@ def create_user_current_project_report(user, project_id=None, from_date=None, to
     serializer = AdvanceTimeEntrySerializerForHistory(queryset, many=True)
     serialized_data = serializer.data
 
+    flag_interval = False
+    if from_date and to_date:
+        from_date = convert_timestamp_to_date(float(from_date))
+        to_date = convert_timestamp_to_date(float(to_date))
+        flag_interval = True
+
     data = []
     if project_id:
+        if flag_interval:
+            report_name = f'{get_project_by_id(project_id).name} {user.name} {from_date} - {to_date}'
+        else:
+            report_name = f'{get_project_by_id(project_id).name} {user.name} {datetime.now().date()}'
+
         titles_list = ['Дата', 'Рабочие часы', 'Аванс', 'Заработная плата', 'Валюта']
 
         for row in serialized_data:
@@ -40,8 +48,12 @@ def create_user_current_project_report(user, project_id=None, from_date=None, to
             salary = round(row['salary'], 4) if row['salary'] else 0
             data.append([date, str(work_time), str(advance), str(salary), str(row['currency'])])
     else:
-        titles_list = ['Дата', 'Рабочие часы', 'Аванс', 'Заработная плата', 'Валюта', 'Проект']
+        if flag_interval:
+            report_name = f'{user.name} {from_date} - {to_date}'
+        else:
+            report_name = f'{user.name} {datetime.now().date()}'
 
+        titles_list = ['Дата', 'Рабочие часы', 'Аванс', 'Заработная плата', 'Валюта', 'Проект']
         for row in serialized_data:
             date = str(convert_timestamp_to_date(row['date']))
             work_time = row['workTime'] if row['workTime'] else 0
@@ -51,11 +63,10 @@ def create_user_current_project_report(user, project_id=None, from_date=None, to
             data.append([date, str(work_time), str(advance), str(salary), str(row['currency']), str(project)])
 
         data = sorted(data, key=itemgetter(5))
-    rand_string = ''.join(random.choice(string.ascii_lowercase) for i in range(32))
-    name = '/users_reports' + f'/{rand_string}.pdf'
-    url = MEDIA_ROOT + name
-    create_pdf_report(titles_list=titles_list, data_list=data, save_path=url)
-    return create_report_object(user=user, date=datetime.now().date(), url=SERV_NAME + '/media' + name)
+
+    create_pdf_report(titles_list=titles_list, data_list=data, report_name=report_name)
+    return create_report_object(user=user, date=datetime.now().date(),
+                                url=SERV_NAME + '/media' + '/users_reports' + '/' + report_name + '.pdf')
 
 
 def create_current_project_report_for_all_workers(user, project_id, from_date=None, to_date=None):
@@ -71,6 +82,17 @@ def create_current_project_report_for_all_workers(user, project_id, from_date=No
     serializer = AdvanceTimeEntrySerializerForHistory(queryset, many=True)
     serialized_data = serializer.data
 
+    flag_interval = False
+    if from_date and to_date:
+        from_date = convert_timestamp_to_date(float(from_date))
+        to_date = convert_timestamp_to_date(float(to_date))
+        flag_interval = True
+
+    if flag_interval:
+        report_name = f'{get_project_by_id(project_id).name} {from_date} - {to_date}'
+    else:
+        report_name = f'{get_project_by_id(project_id).name} {datetime.now().date()}'
+
     titles_list = ['Дата', 'Рабочие часы', 'Аванс', 'Заработная плата', 'Валюта', 'Имя']
 
     data = []
@@ -85,8 +107,6 @@ def create_current_project_report_for_all_workers(user, project_id, from_date=No
 
     data = sorted(data, key=itemgetter(5))
 
-    rand_string = ''.join(random.choice(string.ascii_lowercase) for i in range(32))
-    name = '/users_reports' + f'/{rand_string}.pdf'
-    url = MEDIA_ROOT + name
-    create_pdf_report(titles_list=titles_list, data_list=data, save_path=url)
-    return create_report_object(user=user, date=datetime.now().date(), url=SERV_NAME + '/media' + name)
+    create_pdf_report(titles_list=titles_list, data_list=data, report_name=report_name)
+    return create_report_object(user=user, date=datetime.now().date(),
+                                url=SERV_NAME + '/media' + '/users_reports' + '/' + report_name + '.pdf')
